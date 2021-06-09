96aeafeb653b20333bc5dd8e542b46f05b24ce36
Jonathan Rodrigues <jonrodrigues24@gmail.com>
2021-06-09 10:23:29 -0600

    Extracted double constants and implemented convertF2C.

--- src/main/java/edu/cnm/deepdive/Converter.java
+++ src/main/java/edu/cnm/deepdive/Converter.java
@@ -6,7 +6,9 @@ public class Converter {
   private static final String INPUT_PROMPT = "Please type a Celsius temperature or add \"f\" immediately after your number for fahrenheit (blank to quit):";
   private static final String FAHRENHEIT_OUTPUT_FORMAT = "Fahrenheit temperature: %,.2f degrees%nPlease type a Celsius temperature(blank to quit):";
   private static final String PARSE_EXCEPTION_FORMAT = "unable to parse \"%s\". Please try again: ";
-  public static final String CELSIUS_OUTPUT_FORMAT = "Celsius temperature: %,.2f celsius%nPlease type a Celsius temperature or add \"f\" immediately after your number for fahrenheit (blank to quit):";
+  private static final String CELSIUS_OUTPUT_FORMAT = "Celsius temperature: %,.2f celsius%nPlease type a Celsius temperature or add \"f\" immediately after your number for fahrenheit (blank to quit):";
+  private static final double FAHRENHEIT_PER_CELSIUS = 9.0 / 5;
+  private static final int FAHRENHEIT_FREEZING_POINT = 32;
 
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
@@ -36,12 +38,12 @@ public class Converter {
   public static double convertC2F(double celsius) {
 
 
-    return celsius * 9 / 5 + 32;
+    return celsius * FAHRENHEIT_PER_CELSIUS + FAHRENHEIT_FREEZING_POINT;
 
   }
   public static double convertF2C(double fahrenheit) {
 
-    return 5 * (fahrenheit - 32) / 9; // implemented f2c conversion method
+    return (fahrenheit - FAHRENHEIT_FREEZING_POINT) / FAHRENHEIT_PER_CELSIUS; // implemented f2c conversion method
 
   }
 
